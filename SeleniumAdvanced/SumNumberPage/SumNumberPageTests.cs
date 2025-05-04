c1f26ba36199a10acea95a3b3b8b4a381a773f4e
Alex93777 <130772195+Alex93777@users.noreply.github.com>
2025-05-04 20:22:43 +0300

    SeleniumAdvanced

---
+++ SeleniumAdvanced/SumNumberPage/SumNumberPageTests.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using NUnit.Framework;
+
+
+namespace SumNumberPage
+{
+    public class SumNumberPageTests
+    {
+        private ChromeDriver driver;
+        //private SumNumberPage sumpage;
+
+        [SetUp]
+        public void Setup()
+        {
+            var chromeDriverPath = @"C:\Program Files\ChromeDriver\chromedriver.exe"; // Replace with the actual path
+            driver = new ChromeDriver(chromeDriverPath);
+            //sumpage = new SumNumberPage(driver);
+            //sumpage.OpenPage();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+
+
+        [Test]
+        public void Test_Valid_Numbers()
+        {
+            var sumpage = new SumNumberPage(driver);
+            sumpage.OpenPage();
+            var result = sumpage.AddNumbers("5", "6");
+            Assert.That(result, Is.EqualTo("Sum: 11")); 
+        }
+
+        [Test]
+        public void Test_AddTwoNumbers_Invalid()
+        {
+            var sumpage = new SumNumberPage(driver);
+            sumpage.OpenPage();
+            string resultText = sumpage.AddNumbers("hello", "world");
+            Assert.That(resultText, Is.EqualTo("Sum: invalid input"));
+        }
+
+        [Test]
+        public void Test_AddTwoNumbers_Reset()
+        {
+            SumNumberPage sumNumberPage = new SumNumberPage(driver);
+            sumNumberPage.OpenPage();
+
+            string result = sumNumberPage.AddNumbers("1", "2");
+
+            object asertion = Assert.AreEqual("Sum: 3", result);
+            
+            sumNumberPage.ResetForm();
+        }
+    }
+}
